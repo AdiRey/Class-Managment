e03ed89301d5ce86867997e65ce935e3cb6a511a
AdiRey <160764@stud.prz.edu.pl>
2019-10-07 19:48:56 +0200

    Initial commit

---
+++ src/main/java/pl/javadev/projectjavadev/ProjectjavadevApplication.java
@@ -0,0 +1,13 @@
+package pl.javadev.projectjavadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjavadevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectjavadevApplication.class, args);
+    }
+
+}
